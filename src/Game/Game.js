b569ca27f70910f47a74c41bd83e4fe9237a54a8
Yash Karandikar <ynkarandikar@gmail.com>
2021-12-22 14:55:11 -0800

    add dealer count
    randomly draw cards until count >= 17

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -6,13 +6,36 @@ class Game extends React.Component {
 
     constructor(props) {
         super(props);
+        this._getDealerCount = this._getDealerCount.bind(this);
     }
 
+    // game logic goes here
+    // dealer hits until 17 or more - generate random numbers until count is high enough
+    _getDealerCount() {
+        let count = 0;
+        let val = Math.floor(Math.random() * (10-2) + 2);
+        count = val;
+        while (count < 17) {
+          val = Math.floor(Math.random() * (10-2) + 2);
+          count = count + val;
+        }
+        console.log("final:");
+        console.log(count);
+
+        return count;
+    }
+    
+
     render() {
+
+        let visible = this._getDealerCount()
+
         return(
 
             <div>
-                <p>Game here</p>
+                <p>
+                   {visible} 
+                </p>
             </div>
 
         );
