365dd9ab8d19bc4843126f876162b36a207f3c41
Yash Karandikar <ynkarandikar@gmail.com>
2021-12-23 16:10:48 -0800

    TODO fix player hit - don't change state in render()

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -11,12 +11,12 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            update: false,
+            update: 0,
         }
 
         this._getDealerCount = this._getDealerCount.bind(this);
         this._initial_hit = this._initial_hit.bind(this);
-        this._toggle = this._toggle.bind(this);
+        this._toggleUserHitState = this._toggleUserHitState.bind(this);
     }
 
     // game logic goes here
@@ -29,8 +29,8 @@ class Game extends React.Component {
           val = Math.floor(Math.random() * (10-2) + 2);
           count = count + val;
         }
-        console.log("final:");
-        console.log(count);
+        // console.log("final:");
+        // console.log(count);
 
         return count;
     }
@@ -41,30 +41,30 @@ class Game extends React.Component {
         return Math.floor(Math.random() * (10-2) + 2);
     }
 
-    // obtain initial hit count
-    // add to it
-    // return value every single time button is pressed
-    //_hit_button(original) {
-      //  this.setState({update: true});
-        //let value = original + Math.floor(Math.random() * (10-2) + 2);
-        //console.log(value);
-        // return value;
-    //}
-
-    _toggle() {
-        this.setState({update: true});   
+    _toggleUserHitState() {
+        this.setState({update: this.state.update + 1});   
     }
+
+    _updateUserHitState(visiblePlayer) {
+        // if the state is set to true, add to the hit count and set the state to false again
+        // this allows for the state to switch every time the hit button is clicked
+        if (this.state.update % 2 === 1) {
+            let newValue = Math.floor(Math.random() * (10-2) + 2);
+            console.log(newValue);
+            visiblePlayer = visiblePlayer + newValue;
+            this.setState({update: this.state.update + 1});
+            return visiblePlayer;
+        }
+    }
+
     
     render() {
 
         let visible = this._getDealerCount()
         let visiblePlayer = this._initial_hit()
 
-        // if the state is set to true, add to the hit count and set the state to false again
-        // this allows for the state to switch every time the hit button is clicked
-        if (this.state.update) {
-            visiblePlayer = visiblePlayer + Math.floor(Math.random() * (10-2) + 2);
-            this.setState({update: false});
+        if (this.state.update % 2 === 1) {
+            visiblePlayer = this._updateUserHitState(visiblePlayer);
         }
 
         return(
@@ -76,7 +76,7 @@ class Game extends React.Component {
                 Dealer count: {visible}
                 
                 <img src={blue_chip} alt="hit option" id="blue_chip_hit"
-                onClick={this._toggle}></img>
+                onClick={this._toggleUserHitState}></img>
 
                 Player count: {visiblePlayer}
 
