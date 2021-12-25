103f67c57981b0ba7515965930d449c3c03c0530
Yash Karandikar <ynkarandikar@gmail.com>
2021-12-24 16:08:35 -0800

    fix player hit count
    keep track of dealer and player count with react states and clean up code

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -5,18 +5,19 @@ import blue_chip from '../assets/blue_chip.png';
 
 class Game extends React.Component {
 
-    // set state for hit button click
-    // if button is clicked, add to initial hit count
-
+    // in constructor, keep track of dealer count and player count as states
+    // dealer count and player count are initialized to some value when "Play Game" is selected
     constructor(props) {
         super(props);
+
         this.state = {
-            update: 0,
+            dealerCount: this._getDealerCount(),
+            playerCount: this._initial_hit(),
         }
 
         this._getDealerCount = this._getDealerCount.bind(this);
         this._initial_hit = this._initial_hit.bind(this);
-        this._toggleUserHitState = this._toggleUserHitState.bind(this);
+        this._other_hit = this._other_hit.bind(this);
     }
 
     // game logic goes here
@@ -25,69 +26,47 @@ class Game extends React.Component {
         let count = 0;
         let val = Math.floor(Math.random() * (10-2) + 2);
         count = val;
+
         while (count < 17) {
           val = Math.floor(Math.random() * (10-2) + 2);
           count = count + val;
         }
-        // console.log("final:");
-        // console.log(count);
 
         return count;
     }
 
     // player starts with a random count (2-10)
-    // can hit as needed
+    // this value is initialized when the state is created in the constructor
     _initial_hit() {
         return Math.floor(Math.random() * (10-2) + 2);
     }
 
-    _toggleUserHitState() {
-        this.setState({update: this.state.update + 1});   
+    // if the hit button is clicked,
+    // call this function onClick
+    // in the function, modify the state each time the click occurs
+    _other_hit() {
+        this.setState({playerCount: this.state.playerCount + Math.floor(Math.random() * (10-2) + 2)})
     }
-
-    _updateUserHitState(visiblePlayer) {
-        // if the state is set to true, add to the hit count and set the state to false again
-        // this allows for the state to switch every time the hit button is clicked
-        if (this.state.update % 2 === 1) {
-            let newValue = Math.floor(Math.random() * (10-2) + 2);
-            console.log(newValue);
-            visiblePlayer = visiblePlayer + newValue;
-            this.setState({update: this.state.update + 1});
-            return visiblePlayer;
-        }
-    }
-
     
     render() {
 
-        let visible = this._getDealerCount()
-        let visiblePlayer = this._initial_hit()
-
-        if (this.state.update % 2 === 1) {
-            visiblePlayer = this._updateUserHitState(visiblePlayer);
-        }
-
+        // because we set the dealer & player displays to react states and call them as html objects,
+        // we know that they will modify as defined by this.setState (instead of modifying directly)
         return(
-
             <div>
 
-                <p className="dealer_count">
+                <p id="game_interface">
                 
-                Dealer count: {visible}
+                Dealer count: {this.state.dealerCount}
                 
                 <img src={blue_chip} alt="hit option" id="blue_chip_hit"
-                onClick={this._toggleUserHitState}></img>
+                onClick={this._other_hit}></img>
 
-                Player count: {visiblePlayer}
+                Player count: {this.state.playerCount}
 
-                </p>
-
-                   <p> 
                 </p>
                 
             </div>
-            
-
         );
     }
 
