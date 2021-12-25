c962d1b4e73d4a1fd44e2d2c76d9f1829da8d2c7
Yash Karandikar <ynkarandikar@gmail.com>
2021-12-24 16:27:57 -0800

    clean up app, how to play, and attributions js

--- src/Attributions/Attributions.js
+++ src/Attributions/Attributions.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Attributions.css';
 
-// list citations
+// list citations for other elements used
 class Attributions extends React.Component {
+
     constructor(props) {
         super(props);
 
@@ -13,12 +14,8 @@ class Attributions extends React.Component {
         return(
             <div>
                 <p id="attr_info">
-                All artwork is used without modification with either explicit permission or a Creative Commons license.
-        <li> <a href="https://drawsgood.itch.io/8bit-deck-card-assets">Title screen joker: drawsgood, itch.io (permission)</a></li>
-        <li> <a href="https://georgeblackwell.itch.io/playing-cards-sprite-pack">Playing cards: George Blackwell, itch.io  (Creative Commons Attribution v4.0)</a></li>
-        <li> <a href="https://opengameart.org/content/playing-card-\nassets-52-cards-deck-chips">Poker chips: mehrasaur, OpenGameArt  (CC0 1.0)</a></li>
-        <li> <a href="https://pbarry.itch.io/ability-icons"> Powerup icons: pbarry, itch.io  (permission)</a></li>
-            
+                    All artwork is used without modification with either explicit permission or a Creative Commons license.
+                    <li> <a href="https://opengameart.org/content/playing-card-\nassets-52-cards-deck-chips">Poker chips: mehrasaur, OpenGameArt  (CC0 1.0)</a></li>
                 </p>
             </div>
         );
--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Controls.css';
 
-// list game controls
 class Controls extends React.Component {
+
     constructor(props) {
         super(props);
         
@@ -12,14 +12,9 @@ class Controls extends React.Component {
     render() {
         return (
             <div>
-                <p class="text">
-                    This game is a blackjack variant that provides the player with power-ups to balance against
-        the implicit edge of the dealer. An explanation of each power-up can be found in
-        the "How to Play" section. I hope you have fun playing the game! </p>
-
                 <h2 class="text">How to Play</h2>
-                <p class="text"> Get as close to 21 as possible. Don't go over, or you lose! </p>
-                
+                <p class="text"> Get as close to 21 as possible. Don't go over, or you lose! 
+                To hit, click the blue button.</p>        
             </div>
         );
     }
